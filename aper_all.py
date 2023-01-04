c0dc7e722787d133268a511a7cc2040802ed1419
Karl D. Gordon <kgordon@stsci.edu>
2023-01-04 17:12:28 -0500

    minor updates

--- aper_all.py
+++ aper_all.py
@@ -4,8 +4,7 @@ from aper_one_filter import aper_one_filter
 if __name__ == '__main__':
 
     types = ["HotStars", "ADwarfs", "SolarAnalogs"]
-    filters = ["F560W", "F770W", "F1000W", "F1500W"]
-    filters = ["F1130W"]
+    filters = ["F560W", "F770W", "F1000W", "F1130W", "F1500W"]
 
     for ctype in types:
         for cfilter in filters:
