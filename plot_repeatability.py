a5c3514608ae8e55e71041664fec0766103882d6
Karl D. Gordon <kgordon@stsci.edu>
2023-09-21 14:34:41 -0400

    adding in twice repeated stars

--- plot_repeatability.py
+++ plot_repeatability.py
@@ -47,6 +47,34 @@ if __name__ == "__main__":
         yvals_unc = cfacs[1]
         xvals = cfacs[2]
 
+        # now get the two stars that we repeated twice to fill in the time gap
+
+        for stype, sname in zip(["SolarAnalogs", "ADwarfs"], ["HD 37962", "del UMi"]):
+            cfacs2 = get_calfactors(
+                stype,
+                cfilter,
+                xaxisval="timemid",
+                startday=startday,
+                bkgsub=bkgsub,
+            )       
+            nvals = [pname == sname for pname in cfacs2[4]]
+            if (np.sum(nvals) > 1):
+                nxvals = cfacs2[2][nvals]
+                nyvals = cfacs2[0][nvals]
+                nyvals_unc = cfacs2[1][nvals]
+                #print(sname, cfilter)
+                #print(nyvals)
+                #exit()
+                # find the value of BD+60 1753 that is closest to the last value
+                # use this value to adjust the new star to be on the same scale
+                #    later interpolate between values
+                sindxs = np.argsort(np.absolute(nxvals[-1] - xvals))
+                nyvals *= yvals[sindxs[0]] / nyvals[-1]
+
+                yvals = np.append(yvals, nyvals)
+                yvals_unc = np.append(yvals_unc, nyvals_unc)
+                xvals = np.append(xvals, nxvals)
+
         # fit an exponential
         # ignore the bad data point for F770W
         gvals = abs(xvals - (60070. - startday)) > 20.
@@ -55,12 +83,14 @@ if __name__ == "__main__":
         mod_init = (models.Exponential1D(tau=-200., amplitude=-0.2) 
                     + models.Const1D(amplitude=0.70))
         mod_init[0].amplitude.bounds = [None, 0.0]
-        mod_init[0].tau.fixed = True
+        #mod_init[0].tau.fixed = True
+        mod_init[0].tau.bounds = [-250., -100.]
         # mod_init[1].amplitude.fixed = True
         fitx = xvals[gvals]
         fity = yvals[gvals]
         sindxs = np.argsort(fitx)
         mod_fit = fit(mod_init, fitx[sindxs], fity[sindxs])
+        print(cfilter, mod_fit[0].tau.value)
 
         per_dev = (mod_fit(fitx) - fity) / mod_fit(fitx)
         per_dev = 100.0 * np.sqrt(np.sum(np.square(per_dev) / (len(fitx) - 2)))
@@ -87,12 +117,12 @@ if __name__ == "__main__":
         shifty = 0.05
         ax.text(425., 1. + yoff + shifty, cfilter)
         ax.text(0.0, yoff + shifty + modvals[0],
-                f"A={-1.*per_amp:.1f}% / $\sigma$={per_dev:.1f}%",
+                rf"A={-1.*per_amp:.1f}% / $\tau$={-1.*mod_fit[0].tau.value:.0f} days / $\sigma$={per_dev:.1f}%",
                 backgroundcolor="w",
                 fontsize=0.8*fontsize)
 
     ax.set_ylim(0.9, 3.5)
-    ax.set_xlabel(f"Time [MJD] - {startday}")
+    ax.set_xlabel(f"Time [MJD] - {startday} [days]")
     ax.set_ylabel("Fractional change (+ const)")
 
     plt.tight_layout()
