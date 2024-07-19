3253bc9e6ca70ac129df0f53235dd5a6a3243ccf
Karl Gordon <kgordon@stsci.edu>
2024-07-19 09:31:08 -0400

    upating data table

--- Tables/create_datatable.py
+++ Tables/create_datatable.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     pfile = "Photom/jwst_miri_photom_flight_2jul24.fits"
     cftab = QTable.read(pfile, hdu=1)
     cftab_time = QTable.read(pfile, hdu=2)
-    pixarea = 2.8606325654256E-13
+    pixarea = 2.8606325654256e-13
 
     # fmt: off
     filters = ["F2100W", "F560W", "F770W", "F1000W",
@@ -22,9 +22,14 @@ if __name__ == "__main__":
     otab = QTable(
         names=(
             "name",
+            "PID",
+            "source_type",
             "filter",
             "subarray",
-            "timemid",
+            "time",
+            "central_pixel_rate",
+            "central_pixel_welldepth",
+            "integration_time",
             "inst_flux",
             "inst_flux_unc",
             "inst_background",
@@ -32,37 +37,92 @@ if __name__ == "__main__":
             "flux_unc",
             "background",
         ),
-        units=("", "", "", u.d, "DN/s", "DN/s", "DN/s", u.mJy, u.mJy, u.MJy / u.sr),
-        dtype=("str", "str", "str", "float32", "float32", "float32", "float32", "float32", "float32", "float32"),
+        units=(
+            "",
+            "",
+            "",
+            "",
+            "",
+            u.d,
+            u.DN / u.s,
+            u.DN,
+            u.s,
+            u.DN / u.s,
+            u.DN / u.s,
+            u.DN / u.s,
+            u.mJy,
+            u.mJy,
+            u.MJy / u.sr,
+        ),
+        dtype=(
+            "str",
+            "str",
+            "str",
+            "str",
+            "str",
+            "float32",
+            "float32",
+            "float32",
+            "float32",
+            "float32",
+            "float32",
+            "float32",
+            "float32",
+            "float32",
+            "float32",
+        ),
     )
 
     for cdir in dirs:
         for cfilter in filters:
 
             fname = f"{cdir}/{cfilter}_eefrac0.7_phot.fits"
-            if os.path.isfile(fname): 
+            if os.path.isfile(fname):
 
                 tab = QTable.read(fname)
+                # print(tab.colnames)
+                # exit()
 
                 for cline in tab:
 
                     # compute calibrated flux
-                    gval = (cftab["filter"] == cfilter) & (cftab["subarray"] == cline["subarray"])
+                    gval = (cftab["filter"] == cfilter) & (
+                        cftab["subarray"] == cline["subarray"]
+                    )
                     pipe_cfactor = cftab["photmjsr"][gval][0]
                     pipe_amp = cftab_time["amplitude"][gval][0]
                     pipe_tau = cftab_time["tau"][gval][0]
                     pipe_t0 = cftab_time["t0"][gval][0]
-                    cur_cf = pipe_cfactor + pipe_amp * np.exp((cline["timemid"].value - pipe_t0) / pipe_tau)
-                    phys_flux = (1e9 * cline["aperture_sum_bkgsub"].value * cline["apcorr"] * pixarea * cur_cf) * u.mJy
-                    phys_flux_unc = (1e9 * cline["aperture_sum_bkgsub"].value * cline["apcorr"] * pixarea * cur_cf) * u.mJy
+                    cur_cf = pipe_cfactor + pipe_amp * np.exp(
+                        (cline["timemid"].value - pipe_t0) / pipe_tau
+                    )
+                    phys_flux = (
+                        1e9
+                        * cline["aperture_sum_bkgsub"].value
+                        * cline["apcorr"]
+                        * pixarea
+                        * cur_cf
+                    ) * u.mJy
+                    phys_flux_unc = (
+                        1e9
+                        * cline["aperture_sum_bkgsub"].value
+                        * cline["apcorr"]
+                        * pixarea
+                        * cur_cf
+                    ) * u.mJy
                     phys_bkg = (cline["mean_bkg"].value * cur_cf) * u.MJy / u.sr
 
                     otab.add_row(
                         (
                             cline["name"],
+                            cline["program"],
+                            cdir,
                             cline["filter"],
                             cline["subarray"],
                             cline["timemid"],
+                            cline["pix_max"],
+                            cline["tgroup"] * u.s * cline["ngroups"] * cline["pix_max"],
+                            cline["tgroup"] * cline["ngroups"] * u.s,
                             cline["aperture_sum_bkgsub"],
                             cline["aperture_sum_bkgsub_err"],
                             cline["mean_bkg"],
