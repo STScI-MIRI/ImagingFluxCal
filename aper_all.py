f664571c3840e2da2e4a7a24fff46aba71c2118a
Karl Gordon <kgordon@stsci.edu>
2025-06-27 11:07:08 -0400

    new script and catching up

--- aper_all.py
+++ aper_all.py
@@ -5,12 +5,12 @@ from aper_one_filter import aper_one_filter
 
 if __name__ == '__main__':
 
-    types = ["HotStars", "SolarAnalogs", "ADwarfs"]
-    # types = ["ADwarfs"]
+    # types = ["HotStars", "SolarAnalogs", "ADwarfs"]
+    types = ["ADwarfs"]
     imagefilters = ["F560W", "F770W", "F1000W", "F1130W", "F1280W", "F1500W",
                    "F1800W", "F2100W", "F2550W", "FND"]
     coronfilters = ["F2300C", "F1550C", "F1140C", "F1065C"]
-    coronfilters = []
+    # coronfilters = []
     # filters = ["F770W"]
 
     for ctype in types:
@@ -18,11 +18,11 @@ if __name__ == '__main__':
             if exists(f"{ctype}/{cfilter}/"):
                 print(f"{ctype}/{cfilter}/")
                 aper_one_filter(ctype, cfilter)
-                #if cfilter == "F2550W":
-                aper_one_filter(ctype, cfilter, bkgsub=True)
-                aper_one_filter(ctype, cfilter, indivcals=True)
-                aper_one_filter(ctype, cfilter, indivcals=True, bkgsub=True)
-                aper_one_filter(ctype, cfilter, indivmos=True)
+                if cfilter != "FND":
+                   aper_one_filter(ctype, cfilter, bkgsub=True)
+                   #aper_one_filter(ctype, cfilter, indivcals=True)
+                   #aper_one_filter(ctype, cfilter, indivcals=True, bkgsub=True)
+                   #aper_one_filter(ctype, cfilter, indivmos=True)
         for cfilter in coronfilters:
             if exists(f"{ctype}/{cfilter}/"):
                 print(f"{ctype}/{cfilter}/")
