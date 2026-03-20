41d0cb5035b935ed55acec86cf97a2b3a2d702c0
Karl Gordon <kgordon@stsci.edu>
2026-03-20 17:21:58 -0400

    new RSCD file

--- pipeline_one_filter.py
+++ pipeline_one_filter.py
@@ -110,7 +110,7 @@ if __name__ == "__main__":
     if args.nrscd:
         print("using new rscd parameter file")
         # rscdfile = "RSCD/jwst_miri_rscd_kdg_updated.fits"
-        rscdfile = "RSCD/MIRI_MIRIMAGE_RSCD_10.00.00.fits"
+        rscdfile = "RSCD/MIRI_MIRIMAGE_RSCD_10.00.02.fits"
     else:
         rscdfile = None
 
